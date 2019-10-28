d22ef2eadc9ca2e1810177408c268aa53cb44c6f
jideman2k <jideman2k@yandex.ru>
2019-10-28 01:26:28 -0700

    move index and app to home folder

---
+++ app.js
@@ -0,0 +1,44 @@
+// var name = "John";
+// const lname = "Doe";
+// console.log(name + " " + lname);
+// var name = "Wick";
+// console.log(name + " " + lname);
+// console.log(name + " " + lname);
+// console.log("Hello World from me");
+// console.log(9 + 2);
+// console.log(false || true);
+// console.log(["Ant", "Bird", "Spider"]);
+// console.log({ a: "bird", b: 2, c: false });
+// console.table({ a: "bird", b: 2, c: false, d: [2, 4, 6] });
+// console.error("Enter correct value");
+// console.time("It takes the console to logout");
+// console.log("Hello World from me");
+// console.log(9 + 2);
+// console.log(false || true);
+// console.log(["Ant", "Bird", "Spider"]);
+// console.log({ a: "bird", b: 2, c: false });
+// console.error("Enter correct value");
+// console.warn("This is an error");
+// console.timeEnd("It takes the console to logout");
+// console.clear();
+var greetings;
+console.log(greetings);
+greetings = "Hello Js";
+console.log(greetings + " ✌");
+//const name;//  const cant be initialized
+const person = {
+  name: "John",
+  lname: "Smith",
+  age: 24,
+  profession: "doctor",
+  Ismarried: true
+};
+console.log("Doctor " + this.name + " welcome you to our hospital");
+// To change the value of object property value
+// person.Ismarried = false;
+person.name = "Sara";
+console.log(person);
+person.Ismarried = false;
+// console.log(Ismarried);
+// console.clear();
+console.log("Git is awesome"); //learning to git
